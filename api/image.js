2c6bda8a038a9cd89c4893d21f15c65c0aaa6e55
mbangakh-netizen <mbangakh@gmail.com>
2026-08-09 00:36:12 +0500

    Update image.js

--- api/image.js
+++ api/image.js
@@ -1,4 +1,4 @@
-const IMAGE_URL = "https://scontent.fisb5-2.fna.fbcdn.net/v/t39.84726-6/743796725_1713108693308022_4483855554106468622_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=110&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=92e707&_nc_ohc=RWM1RA03cV8Q7kNvwEaizww&_nc_oc=Ado8qpMGglFtanXrpPgU0OOXeAo3Udqvf4vppJPP0H7xmaRIWvAy9UOh9l4QwkDAUxs&_nc_zt=14&_nc_ht=scontent.fisb5-2.fna&_nc_gid=0GaLvhdWJngOf9NUpStcPw&_nc_ss=7b2a8&oh=00_AQHEj3ibzOtwxQEY9d-74VKdY5ME5RUqd2dGCFgUzgnbvg&oe=6A7D6BC5";
+const IMAGE_URL = "https://scontent.fisb5-1.fna.fbcdn.net/v/t45.5328-4/767131402_2427239634464277_6741396560748133772_n.jpg?_nc_cat=103&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=247b10&_nc_ohc=Ivy_orHbDFYQ7kNvwFupRnp&_nc_oc=Adrho2zGSpYHpuRIVvnyzctPzd2RgTJOUh0qHEAgshHgSLkZJczaGvUN3ImIK-xQooQ&_nc_zt=23&_nc_ht=scontent.fisb5-1.fna&_nc_gid=GNsspj2pP8rl2yLR-tgMIw&_nc_ss=7b2a8&oh=00_AQHsegnZs-9i2L3hb7728Ws10D6h1eV6KYR2YPZWEMCPow&oe=6A7D696B";
 
 module.exports = async (req, res) => {
   try {
